e3ff95915f4f586244dcb170c9e384965327344a
Chamika Dilshan <disanayakedilshan7@gmail.com>
2026-05-20 17:30:35 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+
 export default defineConfig({
   plugins: [react()],
+  base: '/SolarEdge-Solar-Mangement-System/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: { proxy: { '/api': 'http://localhost:5000', '/socket.io': { target: 'http://localhost:5000', ws: true } } }
+  server: { 
+    proxy: { 
+      '/api': 'http://localhost:5000', 
+      '/socket.io': { target: 'http://localhost:5000', ws: true } 
+    } 
+  }
 })
